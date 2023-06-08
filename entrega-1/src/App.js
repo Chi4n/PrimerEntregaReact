85b17ccb28cf1ee7282540f065204027624993c0
GianBaeza <gianb04@gmail.com>
2023-06-07 22:33:34 -0300

    inicio

---
+++ entrega-1/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./componets/Navbar/Navbar";
+
+function App(){
+    return(
+        <body className="#">
+            <Navbar/>
+
+        </body>
+           
+    )
+}
+
+export default App;
